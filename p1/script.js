f2b31dd073233b188b72567958a4330d7facb2e1
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-04-18 16:04:38 -0700

    asdf

--- p1/script.js
+++ p1/script.js
@@ -5,3 +5,10 @@
 function square(num) {
   return num * num;
 }
+
+// ------------------------------------------------------------------
+// Q2 - What is a function Expression
+// after the equal sign this is an anonymous function 
+const subtractTwo = function (num) {
+  return num - 2;
+}
